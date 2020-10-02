4d946fbba4aacce0cec21460b746c2dabec53c7d
taiga10jp <72250649+taiga10jp@users.noreply.github.com>
2020-10-02 17:46:58 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,8 +17,9 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+    
 @bot.command()
-async def LAZ(ctx):
+async def laz(ctx):
     await ctx.send('ｴｰｽｴｰｽ…')
 
     
