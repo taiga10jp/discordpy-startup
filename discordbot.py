41deaf2b6df8b18999ba46df4684f232a11979ef
taiga10jp <72250649+taiga10jp@users.noreply.github.com>
2020-10-02 17:55:47 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -21,6 +21,31 @@ async def ping(ctx):
 @bot.command()
 async def laz(ctx):
     await ctx.send('ｴｰｽｴｰｽ…')
+    
+    
+#coding:UTF-8
+import discord
+from discord.ext import tasks
+from datetime import datetime 
+
+TOKEN = "**********" #トークン
+CHANNEL_ID = ********** #チャンネルID
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+# 60秒に一回ループ
+@tasks.loop(seconds=60)
+async def loop():
+    # 現在の時刻
+    now = datetime.now().strftime('%H:%M')
+    if now == '17:57':
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send('おはよう')  
+
+#ループ処理実行
+loop.start()
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
 
     
 
